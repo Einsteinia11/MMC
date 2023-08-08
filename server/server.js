9d3ee3ebd5baaae316076e7851829692cc6999e8
Einsteinia <informationviakate2004@gmail.com>
2023-08-08 17:33:07 +0530

    connecting server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-const express = require("express");
-const cors = require("cors");
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 const app = express();
-
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-
-app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
-});
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: "root",
+  password: ""
+})
+con.connect(function(err){
+  if(err){
+    console.log("Error in Connection");
+  } else{
+    console.log("Connected")
+  }
+})
+app.listen(8081, ()=> {
+  console.log("Running");
+})
