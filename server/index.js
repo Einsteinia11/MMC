8040a3dc02e0b23931143cd46cbbacabdc6e2c89
Einsteinia <informationviakate2004@gmail.com>
2023-08-11 11:14:01 +0530

    connected to the database

--- server/index.js
+++ server/index.js
@@ -11,14 +11,13 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.send({ message: "Hello from server!" });
 });
-
 const con = mysql.createConnection({
   host: 'localhost',
   user: "root",
   password: "",
-  database: ""
+  database: "client"
 })
 con.connect(function(err){
   if(err){
@@ -28,6 +27,12 @@ con.connect(function(err){
     console.log("Connected")
   }
 })
+app.get("/", (req, res) => {
+  const sqlInsert = "INSERT INTO login_credentials(PID, UserName, Password, Type) values('P#01', 'Nandan', 'Hare krishna', 'Mentor');";
+  con.query(sqlInsert, (err, result) => {
+    res.send("I executed that Maám");
+  });
+});
 app.listen(8081, ()=> {
   console.log("Running");
 })
